3865eb10e3e2a024ee3acc0c30b63b0c6cea2da0
Bernardo <m>
2019-04-29 19:09:18 -0500

    prueba

--- app/src/main/java/com/example/salud_tips/ActividadDetalleArticulo.java
+++ app/src/main/java/com/example/salud_tips/ActividadDetalleArticulo.java
@@ -1,7 +1,5 @@
 package com.example.salud_tips;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
@@ -12,13 +10,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
+
 public class ActividadDetalleArticulo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_detalle_articulo);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_detalle);
         setSupportActionBar(toolbar);
 
